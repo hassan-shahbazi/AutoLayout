242ee9690307b011ba0dd6401f45e3f5512e35fb
Hassan Shahbazi <h-shahbazi@hotmail.com>
2019-12-25 12:34:17 +0200

    Initial Commit

---
+++ AutoLayout/AutoLayout.h
@@ -0,0 +1,19 @@
+//
+//  AutoLayout.h
+//  AutoLayout
+//
+//  Created by Hassan Shahbazi on 25.12.2019.
+//  Copyright © 2019 Hassan Shahbazi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AutoLayout.
+FOUNDATION_EXPORT double AutoLayoutVersionNumber;
+
+//! Project version string for AutoLayout.
+FOUNDATION_EXPORT const unsigned char AutoLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoLayout/PublicHeader.h>
+
+
